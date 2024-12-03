66a505d54caeda2bb47991cf77257c00fe695e02
Huan Nguyen <nguyenvanhuan243@gmail.com>
2024-12-03 22:21:25 +0700

    add create token

--- programs/bonding_curve/src/instructions/mod.rs
+++ programs/bonding_curve/src/instructions/mod.rs
@@ -1,5 +1,6 @@
 pub mod add_liquidity;
 pub mod create_pool;
+pub mod create_token;
 pub mod initialize;
 pub mod remove_liquidity;
 pub mod buy;
@@ -7,6 +8,7 @@ pub mod sell;
 
 pub use add_liquidity::*;
 pub use create_pool::*;
+pub use create_token::*;
 pub use initialize::*;
 pub use remove_liquidity::*;
 pub use buy::*;
